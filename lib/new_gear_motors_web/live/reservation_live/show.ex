88f32c7f7c5c160e72c83fed8a11a1fa79f773e5
Luke Bailey <baileylu@tcd.ie>
2025-05-25 17:59:29 +0100

    Write correct show.ex

--- lib/new_gear_motors_web/live/reservation_live/show.ex
+++ lib/new_gear_motors_web/live/reservation_live/show.ex
@@ -4,58 +4,38 @@ defmodule NewGearMotorsWeb.ReservationLive.Show do
   alias NewGearMotors.Reservations
   alias NewGearMotors.Reservations.Messages
 
+  require Logger
+
+  on_mount {NewGearMotorsWeb.UserAuth, :mount_current_user}
+
   @impl true
   def mount(%{"id" => id}, _session, socket) do
-    reservation =
-      Reservations.get_reservation!(id)
-      |> Reservations.preload_messages()
+    reservation = Reservations.get_reservation!(id)
 
     socket =
       socket
       |> assign(:page_title, page_title(socket.assigns.live_action))
       |> assign(:reservation, reservation)
-      |> stream(:messages, reservation.messages)
 
     {:ok, socket}
   end
 
   @impl true
   def handle_params(params, _url, socket) do
+    socket = socket |> assign(:message_id, nil)
+
     {:noreply, apply_action(socket, socket.assigns.live_action, params)}
   end
 
   defp apply_action(socket, :show, _params), do: socket
   defp apply_action(socket, :edit, _params), do: socket
 
-  defp apply_action(socket, :edit_message, %{"message_id" => message_id}) do
-    message = Messages.get_message!(message_id)
-
+  defp apply_action(socket, :edit_message, params) do
     socket
-    |> assign(:message, message)
-  end
-
-  @impl true
-  def handle_event("delete_message", %{"message_id" => message_id}, socket) do
-    message = Messages.get_message!(message_id)
-    {:ok, _} = Messages.delete_message(message)
-
-    {:noreply, stream_delete(socket, :messages, message)}
-  end
-
-  @impl true
-  def handle_event("new_message", %{"message_text" => message_text}, socket) do
-    {:ok, message} =
-      Messages.create_message(%{
-        text: message_text,
-        from_id: socket.assigns.current_user.id,
-        reservation_id: socket.assigns.reservation.id
-      })
-
-    {:noreply, stream_insert(socket, :messages, message)}
+    |> assign(:message_id, params["message_id"])
   end
 
   defp page_title(:show), do: "Show Reservation"
   defp page_title(:edit), do: "Edit Reservation"
-  defp page_title(:new_message), do: "New Message"
   defp page_title(:edit_message), do: "Edit Message"
 end
