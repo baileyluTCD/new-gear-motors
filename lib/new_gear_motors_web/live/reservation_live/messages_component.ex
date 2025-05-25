69e783c2417a750842c1439c1856e9ecae833c55
Luke Bailey <baileylu@tcd.ie>
2025-05-25 18:00:00 +0100

    Separate out message handling to live component

--- lib/new_gear_motors_web/live/reservation_live/messages_component.ex
+++ lib/new_gear_motors_web/live/reservation_live/messages_component.ex
@@ -6,90 +6,56 @@ defmodule NewGearMotorsWeb.ReservationLive.MessagesComponent do
   alias NewGearMotors.Reservations.Messages.Message
 
   @impl true
-  def render(assigns) do
-    ~H"""
-    <div>
-      <h2 class="text-md font-semibold leading-8 text-zinc-800 mt-10">
-        Messages
-      </h2>
+  def update(%{id: id} = assigns, socket) do
+    messages = Messages.list_messages_for_reservation_id(id)
 
-      <.table id="messages" rows={@messages}>
-        <:col :let={{_id, message}} label="From">{message.from}</:col>
-        <:col :let={{_id, message}} label="Body">{message.text}</:col>
-        <:action :let={{_id, message}}>
-          <.link patch={~p"/reservations/#{@reservation}/messages/#{message}/edit"}>Edit</.link>
-        </:action>
-        <:action :let={{id, message}}>
-          <.link
-            phx-click={JS.push("delete_message", value: %{message_id: message.id}) |> hide("##{id}")}
-            data-confirm="Are you sure?"
-          >
-            Delete
-          </.link>
-        </:action>
-      </.table>
-
-      <.simple_form
-        for={@form}
-        id="messages-form"
-        phx-target={@myself}
-        phx-change="validate"
-        phx-submit="save"
-      >
-        <.input field={@form[:text]} placeholder="Message..." />
-        <:actions>
-          <.button phx-disable-with="Sending...">Send Message</.button>
-        </:actions>
-      </.simple_form>
-    </div>
-    """
-  end
-
-  @impl true
-  def update(%{messages: messages} = assigns, socket) do
-    message = %Message{}
+    empty_message = %Message{}
 
     {:ok,
      socket
      |> assign(assigns)
-     |> assign(:message, message)
+     |> stream(:messages, messages)
+     |> assign(:message, empty_message)
      |> assign_new(:form, fn ->
-       to_form(Messages.change_message(message))
+       to_form(Messages.change_message(empty_message))
      end)}
   end
 
   @impl true
-  def handle_event("validate", %{"message" => message_params}, socket) do
+  def handle_event("delete_message", %{"message_id" => message_id}, socket) do
+    message = Messages.get_message!(message_id)
+    {:ok, _} = Messages.delete_message(message)
+
+    {:noreply, stream_delete(socket, :messages, message)}
+  end
+
+  def handle_event(event, %{"message" => message_params}, socket) do
+    message_params =
+      Map.merge(message_params, %{
+        "from_id" => socket.assigns.current_user.id,
+        "reservation_id" => socket.assigns.reservation.id
+      })
+
+    handle_with_defaults(event, message_params, socket)
+  end
+
+  defp handle_with_defaults("validate", message_params, socket) do
     changeset = Messages.change_message(socket.assigns.message, message_params)
     {:noreply, assign(socket, form: to_form(changeset, action: :validate))}
   end
 
-  def handle_event("save", %{"message" => message_params}, socket) do
+  defp handle_with_defaults("save", message_params, socket) do
     save_message(socket, socket.assigns.action, message_params)
   end
 
-  defp save_message(socket, :edit, message_params) do
-    case Messages.update_message(socket.assigns.message, message_params) do
-      {:ok, message} ->
-        notify_parent({:saved, message})
-
-        {:noreply,
-         socket
-         |> put_flash(:info, "Message updated successfully")
-         |> push_patch(to: socket.assigns.patch)}
-
-      {:error, %Ecto.Changeset{} = changeset} ->
-        {:noreply, assign(socket, form: to_form(changeset))}
-    end
-  end
-
-  defp save_message(socket, :new, message_params) do
+  defp save_message(socket, :show, message_params) do
     case Messages.create_message(message_params) do
       {:ok, message} ->
-        notify_parent({:saved, message})
+        message = message |> Messages.preload_from()
 
         {:noreply,
          socket
+         |> stream_insert(:messages, message)
          |> put_flash(:info, "Message sent successfully")
          |> push_patch(to: socket.assigns.patch)}
 
@@ -97,6 +63,4 @@ defmodule NewGearMotorsWeb.ReservationLive.MessagesComponent do
         {:noreply, assign(socket, form: to_form(changeset))}
     end
   end
-
-  defp notify_parent(msg), do: send(self(), {__MODULE__, msg})
 end
