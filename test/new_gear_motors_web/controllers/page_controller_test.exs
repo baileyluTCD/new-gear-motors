9073ed023d0cef045f36c5f6040420fb0d31ae92
Luke Bailey <baileylu@tcd.ie>
2025-05-29 17:52:51 +0100

    Fix home page integration test

--- test/new_gear_motors_web/controllers/page_controller_test.exs
+++ test/new_gear_motors_web/controllers/page_controller_test.exs
@@ -3,6 +3,7 @@ defmodule NewGearMotorsWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, ~p"/")
-    assert html_response(conn, 200) =~ "Peace of mind from prototype to production"
+    assert html_response(conn, 200) =~ "Built for the road."
+    assert html_response(conn, 200) =~ "Priced for you."
   end
 end
