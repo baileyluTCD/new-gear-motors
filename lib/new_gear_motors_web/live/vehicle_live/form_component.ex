c955eae41382258f74e1a76d130f08dc8612fb81
Luke Bailey <baileylu@tcd.ie>
2025-06-15 22:03:48 +0100

    Integrate file input with the form to provide synced validation

--- lib/new_gear_motors_web/live/vehicle_live/form_component.ex
+++ lib/new_gear_motors_web/live/vehicle_live/form_component.ex
@@ -2,6 +2,7 @@ defmodule NewGearMotorsWeb.VehicleLive.FormComponent do
   use NewGearMotorsWeb, :live_component
 
   alias NewGearMotors.Vehicles
+  alias NewGearMotors.Vehicles.Covers.Cover
 
   @impl true
   def render(assigns) do
@@ -19,11 +20,18 @@ defmodule NewGearMotorsWeb.VehicleLive.FormComponent do
         phx-change="validate"
         phx-submit="save"
       >
-        <.live_file_input upload={@uploads.image} />
-        <img :if={@image_url} src={@image_url} />
-        <.error :if={@image_url_error}>
-          {@image_url_error}
-        </.error>
+        <.live_file_input upload={@uploads.cover} />
+
+        <%= if cover = @form[:cover].value do %>
+          <img src={Cover.url(cover)} alt="Previous Image" />
+        <% end %>
+
+        <%= if err = @form.errors[:cover] do %>
+          <%= if {msg, _} = err do %>
+            <.error>{msg}</.error>
+          <% end %>
+        <% end %>
+
         <.input field={@form[:name]} type="text" label="Name" />
         <.input field={@form[:price]} type="text" label="Price" />
         <.input field={@form[:description]} type="textarea" label="Description" />
@@ -41,36 +49,32 @@ defmodule NewGearMotorsWeb.VehicleLive.FormComponent do
     {:ok,
      socket
      |> assign(assigns)
-     |> allow_upload(:image,
-       accept: ~w(.png .jpg .jpeg),
+     |> allow_upload(:cover,
+       accept: ~w(.png .jpg .jpeg .avif),
        max_entries: 1,
-       progress: &handle_progress/3,
-       auto_upload: true
+       auto_upload: true,
+       progress: &handle_progress/3
      )
-     |> assign(:image_url, nil)
-     |> assign(:image_url_error, nil)
      |> assign_new(:form, fn ->
        to_form(Vehicles.change_vehicle(vehicle))
      end)}
   end
 
-  defp handle_progress(:image, entry, socket) do
+  defp handle_progress(:cover, entry, socket) do
     if entry.done? do
-      uploaded_file =
-        consume_uploaded_entry(socket, entry, fn %{path: path} ->
-          dest = Path.join("priv/static/uploads", Path.basename(path))
-          File.cp!(path, dest)
-          {:ok, dest}
-        end)
-
-      basename = Path.basename(uploaded_file)
+      vehicle_params =
+        socket.assigns.form.params
+        |> with_cover(socket)
 
       socket =
         socket
-        |> put_flash(:info, "File #{basename} uploaded")
-        |> assign(:image_url, ~p"/uploads/#{basename}")
+        |> put_flash(:info, "Image uploaded")
+        |> assign(
+          :form,
+          Map.put(socket.assigns.form, "cover", vehicle_params["cover"])
+        )
 
-      {_, socket} = handle_event("validate", %{"vehicle" => socket.assigns.form.params}, socket)
+      {_, socket} = handle_event("validate", %{"vehicle" => vehicle_params}, socket)
 
       {:noreply, socket}
     else
@@ -80,36 +84,43 @@ defmodule NewGearMotorsWeb.VehicleLive.FormComponent do
 
   @impl true
   def handle_event("validate", %{"vehicle" => vehicle_params}, socket) do
-    {socket, vehicle_params} = inject_image(socket, vehicle_params)
+    vehicle_params =
+      vehicle_params
+      |> with_cover(socket)
 
     changeset = Vehicles.change_vehicle(socket.assigns.vehicle, vehicle_params)
     {:noreply, assign(socket, form: to_form(changeset, action: :validate))}
   end
 
   def handle_event("save", %{"vehicle" => vehicle_params}, socket) do
-    {socket, vehicle_params} = inject_image(socket, vehicle_params)
+    vehicle_params =
+      vehicle_params
+      |> with_cover(socket)
 
-    save_vehicle(socket, socket.assigns.action, vehicle_params)
+    socket
+    |> save_vehicle(socket.assigns.action, vehicle_params)
   end
 
-  defp inject_image(socket, vehicle_params) do
-    if socket.assigns.image_url do
-      vehicle_params =
-        Map.merge(vehicle_params, %{
-          "image_url" => socket.assigns.image_url
-        })
-
-      socket =
-        socket
-        |> assign(:image_url_error, nil)
-
-      {socket, vehicle_params}
+  defp with_cover(vehicle_params, socket) do
+    if socket.assigns.uploads.cover.entries |> Enum.any?(&(!&1.done?)) do
+      vehicle_params
     else
-      socket =
-        socket
-        |> assign(:image_url_error, "image is required")
+      entries =
+        consume_uploaded_entries(socket, :cover, fn meta, entry ->
+          cover = %Plug.Upload{
+            content_type: entry.client_type,
+            filename: entry.client_name,
+            path: meta.path
+          }
 
-      {socket, vehicle_params}
+          updated_params = Map.put(vehicle_params, "cover", cover)
+          {:postpone, updated_params}
+        end)
+
+      case entries do
+        [vehicle_params] -> vehicle_params
+        _ -> vehicle_params
+      end
     end
   end
 
