daf31f9184d2b9fdb1a3b1f9a9c04c65c02a4092
Luke Bailey <baileylu@tcd.ie>
2025-06-16 22:34:36 +0100

    Bump version to 0.6.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule NewGearMotors.MixProject do
   def project do
     [
       app: :new_gear_motors,
-      version: "0.5.0",
+      version: "0.6.0",
       elixir: "~> 1.14",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
