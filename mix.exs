46502addb8e27d0aa8bf0faabb4aa3f944e0778f
Luke Bailey <baileylu@tcd.ie>
2025-06-29 21:47:03 +0100

    Use correct version tag

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule NextGearMotors.MixProject do
   def project do
     [
       app: :next_gear_motors,
-      version: "0.6.0",
+      version: "0.7.2",
       elixir: "~> 1.14",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
