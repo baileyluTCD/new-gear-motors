7432349acd3f667a52fbbeb34df963ee36a661f0
Luke Bailey <baileylu@tcd.ie>
2025-06-29 21:45:42 +0100

    Use correct command for image

--- nix/packages/image.nix
+++ nix/packages/image.nix
@@ -5,5 +5,12 @@
 }:
 pkgs.dockerTools.buildLayeredImage {
   name = "next_gear_motors_image";
-  config.Cmd = [ "${flake.packages.${system}.default}/bin/next_gear_motors" ];
+  tag = "latest";
+
+  contents = [ flake.packages.${system}.default ];
+
+  config.Cmd = [
+    "next_gear_motors"
+    "start"
+  ];
 }
