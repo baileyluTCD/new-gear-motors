c0a33a250c4f9364dc7ea6698676cadeb5a98a34
Luke Bailey <baileylu@tcd.ie>
2025-07-01 12:20:31 +0100

    Create production docker container setup

--- nix/packages/image.nix
+++ nix/packages/image.nix
@@ -3,14 +3,30 @@
   flake,
   system,
 }:
+let
+  nextGearMotors = flake.packages.${system}.default;
+
+  startupScript = pkgs.writeShellScript "entrypoint" ''
+    echo "Starting epmd..."
+    ${pkgs.beamMinimalPackages.erlang}/bin/epmd -daemon
+
+    echo "Running migrations..."
+    ${nextGearMotors}/bin/next_gear_motors eval "NextGearMotors.Release.migrate"
+
+    echo "Starting app..."
+    ${nextGearMotors}/bin/next_gear_motors start
+  '';
+in
 pkgs.dockerTools.buildLayeredImage {
   name = "next_gear_motors_image";
   tag = "latest";
 
-  contents = [ flake.packages.${system}.default ];
+  contents = [
+    nextGearMotors
+    pkgs.beamMinimalPackages.erlang
+  ];
 
   config.Cmd = [
-    "next_gear_motors"
-    "start"
+    "${startupScript}"
   ];
 }
