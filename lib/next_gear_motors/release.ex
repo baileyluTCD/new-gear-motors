3674b4e96e5693cfd7191116fac229a6ee26d540
Luke Bailey <baileylu@tcd.ie>
2025-07-01 23:00:17 +0100

    Add release migrator

---
+++ lib/next_gear_motors/release.ex
@@ -0,0 +1,26 @@
+defmodule NextGearMotors.Release do
+  @moduledoc """
+  Useful functions for running in the release build.
+
+  Handles stuff like:
+  - Migrating the database
+  - Rolling back migrations
+  """
+
+  @app :next_gear_motors
+
+  def migrate do
+    for repo <- repos() do
+      {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :up, all: true))
+    end
+  end
+
+  def rollback(repo, version) do
+    {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :down, to: version))
+  end
+
+  defp repos do
+    Application.load(@app)
+    Application.fetch_env!(@app, :ecto_repos)
+  end
+end
