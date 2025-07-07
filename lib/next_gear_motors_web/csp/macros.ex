89a43814fb59708a6fc88db2f22782759672c8f9
Luke Bailey <baileylu@tcd.ie>
2025-07-07 11:33:07 +0100

    Setup live dashboard csp nonce

--- lib/next_gear_motors_web/csp/macros.ex
+++ lib/next_gear_motors_web/csp/macros.ex
@@ -16,20 +16,24 @@ defmodule NextGearMotorsWeb.CSP.Macros do
 
       If `<asset_host>` is included in the string it will be interpolated to the value of the waffle asset host set at runtime.
       """
-      def csp() do
+      def csp(nonce) when is_binary(nonce) do
         config = Application.get_env(:waffle, :asset_host)
 
         unquote(csp_string)
         |> interpolate_host(config)
+        |> String.replace("<nonce>", nonce)
       end
 
       defp interpolate_host(csp_string, {_, asset_host_var}) do
         asset_host = System.get_env(asset_host_var)
 
-        csp_string |> String.replace("<asset_host>", asset_host || "")
+        csp_string |> String.replace("<asset_host>", asset_host)
       end
 
-      defp interpolate_host(csp_string, nil), do: csp_string
+      defp interpolate_host(csp_string, nil),
+        do:
+          csp_string
+          |> String.replace("<asset_host>", "")
     end
   end
 end
